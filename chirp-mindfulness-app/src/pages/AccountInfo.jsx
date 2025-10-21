6e3fc5fc8e618f55ca55e51f69387e9e1bffb5e9
Rosangely De Los Santos <192157716+rosangely-dls@users.noreply.github.com>
2025-10-21 12:49:47 -0400

    added components and pages folders

---
+++ chirp-mindfulness-app/src/pages/AccountInfo.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AccountInfo() {
+    return <h2>Account Info</h2>;
+}
+
+export default AccountInfo;
---
+++ chirp-mindfulness-app/src/pages/NewEntry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function NewEntry() {
+    return <h2>New Entry</h2>;
+}
+
+export default NewEntry;
